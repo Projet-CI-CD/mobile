de430f8009771ea4a6faf9521f8109d9b1917272
Maxence COEUR <maxence.coeur@etu.univ-lyon1.fr>
2025-04-24 15:52:11 +0200

    test_unitaire

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:ci_cd_mobile/View/HomePage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,5 +14,19 @@ import 'package:ci_cd_mobile/main.dart';
 void main() {
   testWidgets('Default test widget'
       '', (WidgetTester tester) async {
+  });
+
+  testWidgets('HomePage contains AppBar title and FloatingActionButton', (WidgetTester tester) async {
+    // Construire la page
+    await tester.pumpWidget(MaterialApp(home: HomePage()));
+
+    // Vérifier la présence du titre dans l'AppBar
+    expect(find.text('CI/CD Mobile'), findsOneWidget);
+
+    // Vérifier la présence du FloatingActionButton
+    expect(find.byType(FloatingActionButton), findsOneWidget);
+
+    // Vérifier l'icône du bouton flottant
+    expect(find.byIcon(Icons.refresh), findsOneWidget);
   });
 }
